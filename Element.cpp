2a6686d3864ac2e8baeaeb97d0987727a5790994
noame <noame>
2022-01-12 21:57:51 +0200

    element class

--- Element.cpp
+++ Element.cpp
@@ -1,5 +1,42 @@
 //
 // Created by User on 12/01/2022.
 //
-
+#include <cmath>
 #include "Element.h"
+
+Element::Element(int nVal, double *xVector, int m) : n_val(nVal), m(m),
+rank(-1) {
+
+    // deep copy-
+    this->xVector=new double [nVal];
+    for (int i = 0; i < nVal; ++i) {
+        this->xVector[i]=xVector[i];
+    }
+    // create f result
+    funcResult=new double [m];
+
+}
+
+Element::~Element() {
+
+    delete [] funcResult;
+
+}
+
+void Element::func() {
+
+    for (int i = 0; i < m; ++i) {
+        //init e vector;
+        double * e=new double [n_val];
+        for (int j = 0; j < n_val; ++j) {
+            e[j]=i+1;
+        }
+        // sumOf calc
+        double sum=0;
+        for (int k = 0; k < n_val; ++k) {
+            sum+= pow((this->xVector[k]-e[k]),2);
+        }
+        funcResult[i]=sum;
+    }
+
+}
--- Element.h
+++ Element.h
@@ -1,12 +1,34 @@
-//
-// Created by User on 12/01/2022.
-//
 
 #ifndef HOMEWORK5GIT_ELEMENT_H
 #define HOMEWORK5GIT_ELEMENT_H
 
 
 class Element {
+
+    // this class hold element data
+private:
+
+    //size of n_value
+    int n_val;
+    //Line of vector
+    double * xVector;
+
+    // size of m
+    int m;
+
+    //result of the function;
+    double * funcResult;
+
+    // rank of Element
+    int rank;
+
+// constructor
+public:
+    Element(int nVal, double *xVector, int m);
+    // calc the (xVector - e)square
+    void func();
+
+     ~Element();
 
 };
 
