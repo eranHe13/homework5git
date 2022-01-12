f596d1c1749901413f35e89f3cb03ca44f858837
eranHe13 <83641304+eranHe13@users.noreply.github.com>
2022-01-12 21:18:35 +0200

    Container.cpp

--- Container.cpp
+++ Container.cpp
@@ -1,5 +1,43 @@
-//
-// Created by User on 12/01/2022.
-//
 
 #include "Container.h"
+
+
+
+
+Container::Container(double** matrix , int n_elems , int n_values , int n_after_func):
+        n_of_elements(n_elems), n_values(n_values) , n_values_after_func(n_after_func)
+{   for(int i = 0 ; i < n_of_elements ; i++)
+    {   Element* curr = new Element(&matrix[i]);
+        if(elements.size() == 0){
+            elements.insert(elements.begin()  , curr);
+        }
+        elements.push_back(curr);
+    }}
+
+
+void Container::simulates(int n_simulate)
+{
+    for(int i = 0 ; i < n_simulate ; i++)
+    {   resize_elements();
+        //pareToSorting(&elements)
+
+    }
+}
+
+void Container:: resize_elements()
+{   int j ;
+    for(int i =n_of_elements;i < 2*n_of_elements ; i++)
+    {
+        get_random_vector();
+        vector<double> res = mrandomVector + eleents[j];
+        j++;
+    }
+}
+
+void Container::get_random_vector()
+{
+    srand( (unsigned)time( NULL ) );
+    double val;
+    for(int i = 0;i < n_values;i++){
+          val =    (double ) rand()/RAND_MAX << endl; }
+}
--- Container.h
+++ Container.h
@@ -6,19 +6,29 @@
 #define HOMEWORK5GIT_CONTAINER_H
 #import<vector>
 #import"Element.h"
+#include <random>
+#include <time.h>
+#include <iostream>
+
 using namespace std;
 class Element;
 
 
 class Container {
 private:
-    int n_of_elements;
-    int n;
-    int m;
+    int n_of_elements; // mue
+    int n_values; // n
+    int n_values_after_func; //m
     vector <Element*> elements;
     vector <double> randomVector;
 
 
+public:
+    Container(double** matrix , int n_elems , int n_values , int n_after_func);
+
+    void simulates(int n_simulate);
+    void resize_elements();
+    void get_random_vector();
 
 
 };
